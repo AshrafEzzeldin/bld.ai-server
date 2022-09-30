5c763619766768b93addbc043e434eb40b0e165d
Ashraf  Ezz <Ashraf.Teleb99@gmail.com>
2022-09-30 21:03:20 +0200

    Add User and Course models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age=models.IntegerField()
+    birth_date = models.DateField()
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.first_name
